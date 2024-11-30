f5c82ab58378d85b26202f943b168eb94ed0c4cb
ihsankhwaritsmi <alkhwaritsmi@gmail.com>
2024-12-01 00:16:27 +0700

    modified:   README.md 	new file:   img/Screenshot from 2024-12-01 00-15-04.png 	modified:   src/Airplane.tsx 	new file:   src/control.ts

--- src/Airplane.tsx
+++ src/Airplane.tsx
@@ -2,8 +2,19 @@ import { useRef } from 'react';
 import { useGLTF } from '@react-three/drei';
 import { useFrame } from '@react-three/fiber';
 import { Matrix4, Vector3, Group, Mesh } from 'three';
+import { updatePlaneAxis } from './control';
+import { Quaternion } from 'three';
+
+// Vector for controlling the plane
+const x = new Vector3(1, 0, 0);
+const y = new Vector3(0, 1, 0);
+const z = new Vector3(0, 0, 1);
 
 export const planePosition = new Vector3(0, 3, 7);
+
+// This is to make the plane rotation slightly delayed
+const delayedRotMatrix = new Matrix4();
+const delayedQuaternion = new Quaternion();
 
 export function Airplane(props: any) {
   const { nodes, materials } = useGLTF('/assets/models/airplane.glb');
@@ -11,21 +22,41 @@ export function Airplane(props: any) {
   const helixMeshRef = useRef<Mesh>();
 
   useFrame(({ camera }) => {
-    // Uncomment this line if you want to move the plane
+    // Uncomment this line if you want to move the plane forward
     // planePosition.add(new Vector3(0, 0, -0.005));
 
-    const matrix = new Matrix4().multiply(
-      new Matrix4().makeTranslation(
-        planePosition.x,
-        planePosition.y,
-        planePosition.z,
-      ),
-    );
+    updatePlaneAxis(x, y, z, planePosition, camera);
+
+    const rotMatrix = new Matrix4().makeBasis(x, y, z);
+
+    const matrix = new Matrix4()
+      .multiply(
+        new Matrix4().makeTranslation(
+          planePosition.x,
+          planePosition.y,
+          planePosition.z,
+        ),
+      )
+      .multiply(rotMatrix);
+
     if (groupRef.current) {
       groupRef.current.matrixAutoUpdate = false;
       groupRef.current.matrix.copy(matrix);
       groupRef.current.matrixWorldNeedsUpdate = true;
     }
+
+    var quatA = new Quaternion().copy(delayedQuaternion);
+    var quatB = new Quaternion();
+    quatB.setFromRotationMatrix(rotMatrix);
+
+    var interpolationFactor = 0.175;
+    var interpolatedQuaternion = new Quaternion().copy(quatA);
+    interpolatedQuaternion.slerp(quatB, interpolationFactor);
+    delayedQuaternion.copy(interpolatedQuaternion);
+
+    delayedRotMatrix.identity();
+    delayedRotMatrix.makeRotationFromQuaternion(delayedQuaternion);
+
     const cameraMatrix = new Matrix4()
       .multiply(
         new Matrix4().makeTranslation(
@@ -33,7 +64,9 @@ export function Airplane(props: any) {
           planePosition.y,
           planePosition.z,
         ),
-      ) // Move the camera at the center of the plane
+      )
+      .multiply(delayedRotMatrix)
+      // Move the camera at the center of the plane
       .multiply(new Matrix4().makeRotationX(-0.2)) // Rotate the camera slightly downward with an angle of depression
       .multiply(new Matrix4().makeTranslation(0, 0.015, 0.3)); // Move the camera at the back of the plane
     // Basically what this does is move the camera so that we have a Third Person View of the plane
---
+++ src/control.ts
@@ -0,0 +1,82 @@
+export let controls: any = {};
+
+
+function easeOutQuad(x : any) {
+    return 1 - (1 - x) * (1 - x);
+  }
+
+window.addEventListener('keydown', (e) => {
+    controls[e.key.toLowerCase()] = true;
+});
+
+window.addEventListener('keyup', (e) => {
+    controls[e.key.toLowerCase()] = false;
+});
+
+let yawVelocity = 0;
+let pitchVelocity = 0;
+let maxVelocity = 0.04;
+
+// let rollVelocity = 0;
+let planeSpeed = 0.006;
+
+export let turbo = 0;
+export function updatePlaneAxis(x: any, y: any, z: any, planePosition: any, camera: any) {
+    yawVelocity *= 0.95;
+    pitchVelocity *= 0.95;
+
+    if(Math.abs(yawVelocity) > maxVelocity) {
+        yawVelocity = Math.sign(yawVelocity) * maxVelocity;
+    }
+    if(Math.abs(pitchVelocity) > maxVelocity) {
+        pitchVelocity = Math.sign(pitchVelocity) * maxVelocity;
+    }
+
+    if (controls['w']) {
+       pitchVelocity -= 0.0025;
+    }
+    if (controls['s']) {
+        pitchVelocity += 0.0025;
+    }
+    if (controls['a']) {
+        yawVelocity += 0.0025;
+    }
+    if (controls['d']) {
+       yawVelocity -= 0.0025;
+    }
+
+    if(controls['r']) {
+        yawVelocity = 0;
+        pitchVelocity = 0;
+        turbo = 0;
+        x.set(1, 0, 0);
+        y.set(0, 1, 0);
+        z.set(0, 0, 1);
+        planePosition.set(0, 3, 7);
+    }
+
+    x.applyAxisAngle(z, yawVelocity);
+    y.applyAxisAngle(z, yawVelocity);
+
+    y.applyAxisAngle(x, pitchVelocity);
+    z.applyAxisAngle(x, pitchVelocity);
+
+    x.normalize();
+    y.normalize();
+    z.normalize();
+
+
+    if(controls.shift) {
+        turbo += 0.025;
+    }else{
+        turbo -= 0.95;
+    }
+    turbo = Math.min(Math.max(turbo, 0), 1);
+
+    let turboSpeed = easeOutQuad(turbo) * 0.02;
+
+    camera.fov = 45 + turboSpeed * 900;
+    camera.updateProjectionMatrix();
+
+    planePosition.add(z.clone().multiplyScalar(-planeSpeed - turboSpeed));
+}
