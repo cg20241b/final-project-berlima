9629758a2c87d907b04b1917b5ad72d5e936e1ef
ihsankhwaritsmi <alkhwaritsmi@gmail.com>
2024-11-30 22:33:41 +0700

    modified:   README.md 	new file:   image.png 	new file:   img/Screenshot from 2024-11-30 16-33-59.png 	new file:   img/Screenshot from 2024-11-30 17-51-55.png 	new file:   img/Screenshot from 2024-11-30 21-24-56.png 	new file:   img/Screenshot from 2024-11-30 21-40-35.png 	modified:   package-lock.json 	modified:   package.json 	new file:   public/assets/models/airplane.glb 	new file:   public/assets/models/scene.glb 	new file:   public/assets/textures/envmap.hdr 	new file:   public/assets/textures/envmap.jpg 	deleted:    public/vite.svg 	new file:   src/Airplane.tsx 	deleted:    src/App.css 	modified:   src/App.tsx 	new file:   src/Landscape.tsx 	new file:   src/SphereEnv.tsx 	deleted:    src/assets/react.svg 	modified:   src/index.css 	modified:   src/main.tsx 	modified:   tsconfig.json 	modified:   src/Airplane.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,39 @@
-import './App.css';
-
-import { useState } from 'react';
-
-import viteLogo from '/vite.svg';
-
-import reactLogo from './assets/react.svg';
+import {
+  PerspectiveCamera,
+  Environment,
+  OrbitControls,
+} from '@react-three/drei';
+import { SphereEnv } from './SphereEnv';
+import { Landscape } from './Landscape';
+import { Airplane } from './Airplane';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href='https://vite.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
+      <SphereEnv />
+      <Environment background={false} files={'assets/textures/envmap.hdr'} />
+
+      <PerspectiveCamera makeDefault position={[0, 10, 10]} />
+      {/* <OrbitControls target={[0, 0, 0]} /> */}
+
+      <Landscape />
+      <Airplane />
+
+      <directionalLight
+        castShadow
+        color={'#f3d29a'}
+        intensity={2}
+        position={[10, 5, 4]}
+        shadow-bias={-0.0005}
+        shadow-mapSize-width={1024}
+        shadow-mapSize-height={1024}
+        shadow-camera-near={0.01}
+        shadow-camera-far={20}
+        shadow-camera-top={6}
+        shadow-camera-bottom={-6}
+        shadow-camera-left={-6.2}
+        shadow-camera-right={6.4}
+      />
     </>
   );
 }
