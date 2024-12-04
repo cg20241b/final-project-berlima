f925ff8aadc0e99461c8107a4fa8395bcd54f578
Ainun Nadhifah Syamsiyah <ainunsyamsiyah@gmail.com>
2024-12-04 13:56:05 +0700

    feat: add loading screen

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,18 @@ import './index.css';
 
 import { Canvas } from '@react-three/fiber';
 import { Suspense } from 'react';
-// import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import App from './App.tsx';
+import LoadingScreen from './Loading.tsx';
 
 createRoot(document.getElementById('root')!).render(
   //so it can cast shadows
-  <Canvas shadows>
-    <Suspense fallback={null}>
-      <App />
-    </Suspense>
-  </Canvas>,
+  <>
+    <Canvas shadows>
+      <Suspense fallback={<LoadingScreen />}>
+        <App />
+      </Suspense>
+    </Canvas>
+  </>,
 );
