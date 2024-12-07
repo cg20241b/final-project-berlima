047d70caa5231a54e2ae211c831968feba073d97
ihsankhwaritsmi <117936462+ihsankhwaritsmi@users.noreply.github.com>
2024-12-07 13:50:52 +0000

    modified:   src/control.ts

--- src/control.ts
+++ src/control.ts
@@ -14,12 +14,15 @@ window.addEventListener('keyup', (e) => {
   controls[e.key.toLowerCase()] = false;
 });
 
+let rollVelocity = 0;
 let yawVelocity = 0;
 let pitchVelocity = 0;
 const maxVelocity = 0.04;
 
 // let rollVelocity = 0;
-const planeSpeed = 0.006;
+let planeSpeed = 0.006;
+let isPaused = false;
+let curr:any;
 
 export let turbo = 0;
 export function updatePlaneAxis(
@@ -29,60 +32,86 @@ export function updatePlaneAxis(
   planePosition: Vector3,
   camera: PerspectiveCamera,
 ) {
-  yawVelocity *= 0.95;
-  pitchVelocity *= 0.95;
 
-  if (Math.abs(yawVelocity) > maxVelocity) {
-    yawVelocity = Math.sign(yawVelocity) * maxVelocity;
+  if(controls['p'] && isPaused===false){
+    isPaused = true;
+    planeSpeed = 0;
+    planePosition.set(curr.x, curr.y, curr.z)
   }
-  if (Math.abs(pitchVelocity) > maxVelocity) {
-    pitchVelocity = Math.sign(pitchVelocity) * maxVelocity;
+  else if(controls['p'] && isPaused===true){
+    isPaused = false;
+    planeSpeed = 0.006
+    planeSpeed = 0;
   }
-
-  if (controls['w']) {
-    pitchVelocity -= 0.0025;
-  }
-  if (controls['s']) {
-    pitchVelocity += 0.0025;
-  }
-  if (controls['a']) {
-    yawVelocity += 0.0025;
-  }
-  if (controls['d']) {
-    yawVelocity -= 0.0025;
-  }
-
-  if (controls['r']) {
-    yawVelocity = 0;
-    pitchVelocity = 0;
-    turbo = 0;
-    x.set(1, 0, 0);
-    y.set(0, 1, 0);
-    z.set(0, 0, 1);
-    planePosition.set(0, 3, 7);
-  }
-
-  x.applyAxisAngle(z, yawVelocity);
-  y.applyAxisAngle(z, yawVelocity);
-
-  y.applyAxisAngle(x, pitchVelocity);
-  z.applyAxisAngle(x, pitchVelocity);
-
-  x.normalize();
-  y.normalize();
-  z.normalize();
-
-  if (controls.shift) {
-    turbo += 0.025;
-  } else {
-    turbo -= 0.95;
-  }
-  turbo = Math.min(Math.max(turbo, 0), 1);
-
-  const turboSpeed = easeOutQuad(turbo) * 0.02;
-
-  camera.fov = 45 + turboSpeed * 900;
-  camera.updateProjectionMatrix();
-
-  planePosition.add(z.clone().multiplyScalar(-planeSpeed - turboSpeed));
+  else{
+    rollVelocity *= 0.95;
+    pitchVelocity *= 0.95;
+    yawVelocity *= 0.95;
+  
+    if (Math.abs(rollVelocity) > maxVelocity) {
+      rollVelocity = Math.sign(rollVelocity) * maxVelocity;
+    }
+    if (Math.abs(pitchVelocity) > maxVelocity) {
+      pitchVelocity = Math.sign(pitchVelocity) * maxVelocity;
+    }
+  
+    if(controls['q']){
+      yawVelocity += 0.0025;
+    }
+    
+    if(controls['e']){
+      yawVelocity -= 0.0025;
+    }
+  
+    if (controls['w']) {
+      pitchVelocity -= 0.0025;
+    }
+    if (controls['s']) {
+      pitchVelocity += 0.0025;
+    }
+    if (controls['a']) {
+      rollVelocity += 0.0025;
+    }
+    if (controls['d']) {
+      rollVelocity -= 0.0025;
+    }
+  
+    if (controls['r']) {
+      rollVelocity = 0;
+      pitchVelocity = 0;
+      turbo = 0;
+      x.set(1, 0, 0);
+      y.set(0, 1, 0);
+      z.set(0, 0, 1);
+      planePosition.set(0, 3, 7);
+    }
+  
+    x.applyAxisAngle(z, rollVelocity);
+    y.applyAxisAngle(z, rollVelocity);
+  
+    y.applyAxisAngle(x, pitchVelocity);
+    z.applyAxisAngle(x, pitchVelocity);
+  
+    x.applyAxisAngle(y, yawVelocity);
+    z.applyAxisAngle(y, yawVelocity);
+  
+    x.normalize();
+    y.normalize();
+    z.normalize();
+  
+    if (controls.shift) {
+      turbo += 0.025;
+    } else {
+      turbo -= 0.95;
+    }
+    turbo = Math.min(Math.max(turbo, 0), 1);
+  
+    const turboSpeed = easeOutQuad(turbo) * 0.02;
+  
+    camera.fov = 45 + turboSpeed * 900;
+    camera.updateProjectionMatrix();
+  
+    curr = planePosition.add(z.clone().multiplyScalar(-planeSpeed - turboSpeed));
+    console.log(curr)
+  } 
 }
